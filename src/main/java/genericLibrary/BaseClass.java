1eaa33873d131f127ea04caa228769c52895c374
sasikalagithub <sasikalaManthena28@gmail.com>
2023-08-10 18:35:21 +0530

    maven changes

--- src/main/java/genericLibrary/BaseClass.java
+++ src/main/java/genericLibrary/BaseClass.java
@@ -8,15 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	
 	public WebDriver driver;
+	
 	public Propertyfile pdata=new Propertyfile();
 	public WebDriverUtilities utilies=new WebDriverUtilities();
-	
 	@BeforeMethod
 	public void openApp() throws IOException {
 	WebDriverManager.chromedriver().setup();
@@ -30,7 +29,8 @@ public class BaseClass {
 		int status = res.getStatus();
 	     String name = res.getName();
 	     if(status==2) {
-	    Photo p=new Photo();
+	    //Photo p=new Photo();
+	    Photo p=new Photo();	
 	    p.getPhoto(driver, name);
 	     }
 		driver.quit();
